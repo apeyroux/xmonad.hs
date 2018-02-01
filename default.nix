de7824b74455e373b944d7f4c87f3208f4afe6e6
Alexandre Peyroux <alex@xn--wxa.email>
2018-02-01 16:36:58 +0100

    build with cabal2nix

--- default.nix
+++ default.nix
@@ -1,16 +1,5 @@
-with import <nixpkgs> { };
+let
+  pkgs = import <nixpkgs> { };
 
-haskell.lib.buildStackProject rec {
-  name = "xmonad-px-${version}";
-  version = "1.0";
-  ghc = haskell.compiler.ghc802;
-  buildInputs = [
-    # haskellPackages.xmobar
-    # haskellPackages.xmonad
-    # haskellPackages.xmonad-contrib
-    # haskellPackages.xmonad-extras
-    stack
-  ];
-  isExecutable = true;
-  src = ./.;
-}
+in
+  pkgs.haskellPackages.callPackage ./xmonad-px.nix { }
---
+++ xmonad-px.nix
@@ -0,0 +1,14 @@
+{ mkDerivation, base, containers, stdenv, taffybar, xmonad
+, xmonad-contrib, xmonad-extras
+}:
+mkDerivation {
+  pname = "xmonad-px";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base containers taffybar xmonad xmonad-contrib xmonad-extras
+  ];
+  license = stdenv.lib.licenses.unfree;
+}
